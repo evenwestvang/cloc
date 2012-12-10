637b681cf2fdf34ef86dcd63ba37e57b4145529a
Even Westvang <even@westvang.com>
2012-12-10 20:32:54 +0100

    Gamma correction

--- lightkeeper.py
+++ lightkeeper.py
@@ -6,22 +6,32 @@ from spi import *
 from lightsource import *
 from numpy import *
 
+
 class LightKeeper:
 
     NUMBER_OF_LIGHTS = 32
+    BYTES_PER_LIGHT = 3
 
     def __init__(self):
 
-        self.transmitter = Spi()
-        # self.transmitter = Null()
+        # self.transmitter = Spi()
+        self.transmitter = Null()
+        self.calculate_gamma()
+
         self.lights = []
 
         self.lights.append(MainProgressLight())
         self.lights.append(MinuteProgressLight(self.NUMBER_OF_LIGHTS))
         self.lights.append(SecondProgressLight(self.NUMBER_OF_LIGHTS))
 
+    def calculate_gamma(self):
+        self.gamma_correction = bytearray(256)
+        for i in range(256):
+            self.gamma[i] = int(pow(float(i) / 255.0, 2.5) * 255.0)
+
     def update(self, progress):
-        self.rgb_array = zeros((self.NUMBER_OF_LIGHTS + 1) * 3, 'B')
+        self.rgb_array = zeros((self.NUMBER_OF_LIGHTS + 1) * self.BYTES_PER_LIGHT, 'B')
+#        self.rgb_array = bytearray(32 * 3)
 
         for light in self.lights:
             light.update(progress)
@@ -31,10 +41,10 @@ class LightKeeper:
         self.set_buffer()
 
     def set_rgb_values(self, position, rgb_tuple):
-        offset = position * 3
-        self.rgb_array[offset + 0] += rgb_tuple[0]
-        self.rgb_array[offset + 1] += rgb_tuple[1]
-        self.rgb_array[offset + 2] += rgb_tuple[2]
+        offset = position * self.BYTES_PER_LIGHT
+        self.rgb_array[offset + 0] += self.gamma[rgb_tuple[0]]
+        self.rgb_array[offset + 1] += self.gamma[rgb_tuple[1]]
+        self.rgb_array[offset + 2] += self.gamma[rgb_tuple[2]]
 
     def set_buffer(self):
         self.transmitter.transmit(self.rgb_array)
--- spi.py
+++ spi.py
@@ -1,18 +1,9 @@
-from quick2wire.spi import *
 from time import *
 
 class Spi:
     def __init__(self):
-        # self.spi0 = SPIDevice(0, 0)
-        # print ("current speed Hz: " + str(self.spi0.speed_hz))
-        # self.spi0.speed_hz = 10000   # 500Khz
-        # print ("current speed Hz: " + str(self.spi0.speed_hz))
-        # self.spi0.clock_mode = SPI_MODE_1 # | SPI_NO_CS 
         self.spidev = file('/dev/spidev0.0', "wb")
 
     def transmit(self, data):
-#            self.spi0.transaction(writing_bytes(0x00, i * 10, i * 10,i * 2))
-#            self.spi0.transaction(writing_bytes(0x00, 0, 0))
-        # self.spi0.transaction(writing_bytes(data))
         self.spidev.write(data)
         self.spidev.flush()
